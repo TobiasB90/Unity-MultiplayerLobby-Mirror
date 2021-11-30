d18aa7a9e20eb15779be8c5161d7ae772ad6030b
Tobias Behr <tobias.behr@gmx.net>
2021-11-30 16:46:07 +0100

    Add UI bugfixes

--- Assets/Scripts/UI/DebugUI.cs
+++ Assets/Scripts/UI/DebugUI.cs
@@ -4,7 +4,18 @@ using UnityEngine;
 public class DebugUI : MonoBehaviour
 {
     [SerializeField] NetworkRoomManagerExtended roomMng;
-
+    public static DebugUI singleton { get; private set; }
+    private void Awake()
+    {
+        if (singleton != null && singleton != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            singleton = this;
+        }
+    }
     void Start()
     {
         DontDestroyOnLoad(this.gameObject);
